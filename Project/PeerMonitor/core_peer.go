e4a90fade345d7f2326809dcb261604340888aff
eirikheir <eiriheir@stud.ntnu.no>
2026-02-18 17:12:02 +0100

    changes in types, shell core

--- Project/PeerMonitor/core_peer.go
+++ Project/PeerMonitor/core_peer.go
@@ -1,24 +1,24 @@
 package peermonitor
 
-import "time"
+import (
+	ordersync "Project/OrderSync"
+	"time"
+)
 
 // core PeerMonitor
 
-
-
-func HandleHeartbeats(state PeerState, hb HeartbeatMsg, now time.Time) PeerState{
+func HandleHeartbeats(peerList []Peer, hb ordersync.NetMsg, now time.Time) ([]Peer, bool) {
 	// Update or create peer, set Alive, update cab calls, set LastSeen
 }
 
-func CheckTimeouts(state PeerState, now time.Time, timeout time.Duration) PeerState {
+func CheckTimeouts(peerList []Peer, now time.Time, timeout time.Duration) ([]Peer, bool) {
 	// Mark peers as Dead if LastSeen + timeout < now
 }
 
-func ToPeerUpdate(state PeerState) PeerUpdate{
+func ToPeerUpdate(peerList []Peer) PeerUpdate {
 	//Convert internal state to output format
 }
 
+func SendhbTx(hbTx chan<- RecoveryMsg, message RecoveryMsg) {
 
-
-
-
+}
--- Project/PeerMonitor/types_peer.go
+++ Project/PeerMonitor/types_peer.go
@@ -15,7 +15,6 @@ const ( // Status is Dead/Alive
 	Alive
 )
 
-
 type Peer struct { //Peer representsa network peer
 	ID             ElevID         // elevator ID is based on the port i comes from since each IP is the same
 	Status         Status         //Dead or alive
@@ -23,11 +22,7 @@ type Peer struct { //Peer representsa network peer
 	LastSeen       time.Time      // When we last received a heartbeat from this peer
 }
 
-type PeerState struct {  // Internal state for core functions
-	Peers map[ElevID]*Peer
-}
-
-type HeartbeatMsg struct { //HeartbeatMsg is recieved from other alive Peers
+type RecoveryMsg struct { //HeartbeatMsg is recieved from other alive Peers
 	SenderID ElevID
 	CabCalls [N_Floors]bool // Current cab calls to be backed up by others
 }
@@ -41,13 +36,12 @@ type PeerConfig struct {
 	TickPeriod time.Duration // 0 => expects external tick when testing, >0 => creates internal ticker in production
 }
 
-type PeerInputs struct {
-	Heartbeat <-chan HeartbeatMsg // Received from TransportUDP Rx
-	Tick      <-chan time.Time    // External tick, checks for dead peers //For Testing
-}
+// type PeerInputs struct {
+// 	Heartbeat <-chan HeartbeatMsg // Received from TransportUDP Rx
+// 	Tick      <-chan time.Time    // External tick, checks for dead peers //For Testing
+// }
 
-type PeerOutputs struct {
-	Update              chan<- PeerUpdate // Sends updated list to OrderSync
-	TransmitBackupCalls chan<- Peer       // Sends backup cab calls
-}
-
+// type PeerOutputs struct {
+// 	Update              chan<- PeerUpdate // Sends updated list to OrderSync
+// 	TransmitBackupCalls chan<- HeartbeatMsg     // Sends backup cab calls
+// }
