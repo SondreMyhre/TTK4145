b6bc0a98c214d87909650e9f263ea655a8b03bb3
mariusdevo <mariusdevo@gmail.com>
2026-03-19 11:20:59 +0100

    Removed unnecessary commenting

--- project/networking/networking_shell.go
+++ project/networking/networking_shell.go
@@ -1,11 +1,5 @@
 package networking
 
-// REMEMBER:
-// maps need to have string-keys to be broadcasted
-// all structs to be sent must have public members
-// broadcast-ip defined in bcast.go is different for personalPCs and labPC
-// broadcast-ip on labpc: 10.100.23.255
-
 import (
 	"fmt"
 	config "project/config"
